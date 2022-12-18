534299a2556dc9397cffd9d61389b76a59f088d6
Xavier Friesen <Gebruiker@Laptop-Xavier.INTERAL.local>
2022-12-18 09:29:20 +0100

    plot3 final

---
+++ plot3.R
@@ -0,0 +1,9 @@
+#plot 2
+png("plot3.png")
+with(data, 
+     plot(Time, Sub_metering_1, type="l", xlab = "", ylab = "Energy sub metering")) +
+     lines(data$Time, data$Sub_metering_2, col = "red") + 
+     lines(data$Time, data$Sub_metering_3, col = "blue") 
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, col = c('black', "red", "blue"))
+dev.off()
